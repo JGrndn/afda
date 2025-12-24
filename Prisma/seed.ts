f5f8f77ee5b138079dd3768698fdaf0c40fe12e2
JGrndn <487007+JGrndn@users.noreply.github.com>
2025-12-24 13:10:02 +0100

    Create schema + seed

--- Prisma/seed.ts
+++ Prisma/seed.ts
@@ -1,5 +1,5 @@
 import 'dotenv/config';
-import { PrismaClient } from '@/generated/prisma/client';
+import { Prisma, PrismaClient } from '@/generated/prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
 
 const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
@@ -8,12 +8,23 @@ const prisma = new PrismaClient({ adapter });
 async function main(){
   console.log('🌱 Starting seed...');
 
-    // ==================== CLEAN DATABASE ====================
-  console.log('🧹 Cleaning database...');
-  
+  await clearDatabase();
   console.log('✅ Database cleaned');
+  
+  const seasonsData : Prisma.SeasonCreateInput[] = [
+    { startYear: 2023, endYear: 2024, isActive: false, membershipAmount: 10, totalDonations: 0 },
+    { startYear: 2024, endYear: 2025, isActive: false, membershipAmount: 20, discountPercent: 5, totalDonations: 0 },
+    { startYear: 2025, endYear: 2026, isActive: true, membershipAmount: 50, discountPercent:10, totalDonations: 0 },
+  ];
+  const seasons = await prisma.season.createManyAndReturn({data : seasonsData });
+  console.log('✅ Seasons created');
 }
 
+async function clearDatabase(){
+  await prisma.$transaction([
+    prisma.season.deleteMany(),
+  ]);
+}
 
 main()
   .catch((e) => {
